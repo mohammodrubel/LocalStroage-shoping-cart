ed035d1cd3d0dab091cd79c01d3e9bad803b7e1c
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-09-09 23:11:48 +0600

    Add files via upload

---
+++ New folder/js/app.js
@@ -0,0 +1,56 @@
+const addItem = ()=>{
+    const inputFeld = document.getElementById('inputFeld')
+    const productName = inputFeld.value 
+        if (productName == ''){
+            return
+        }
+    // display show product 
+    displayProduct(productName)
+    // add to localStorage
+    addProductToCart(productName)
+    inputFeld.value = '';
+}
+
+// DISPLAY PRODUCT LIST 
+const displayProduct = productName =>{
+    const displayProduct = document.getElementById('displayProduct')
+    const li = document.createElement('li')
+    li.classList.add('list-group-item')
+    li.innerText=productName;
+    displayProduct.appendChild(li)
+}
+// LOCALSTORAGE 
+const getCart = ()=>{
+    const cart = localStorage.getItem('cart')
+    let cartObject;
+        if (cart){
+            cartObject = JSON.parse(cart)
+        }else{
+            cartObject = {}
+        }
+    return cartObject
+}
+
+const addProductToCart = productName =>{
+    const cart = getCart()
+        if (cart[productName]){
+            cart [productName] = cart [productName] + 1;
+        }else{
+            cart [productName] = 1;
+        }
+    const cartStringfy = JSON.stringify(cart)
+    localStorage.setItem('cart',cartStringfy)
+}
+// LOAD LOCALSTORAGE DATA SHOW DISPLAY 
+const loadCard = ()=>{
+    const cart = getCart()
+    for (const product in cart){
+        displayProduct(product)
+    }
+}
+loadCard()
+// PLACE ORDER OR RESET 
+const placeOrder = ()=>{
+    document.getElementById('displayProduct').innerText='';
+    localStorage.removeItem('cart')
+}
